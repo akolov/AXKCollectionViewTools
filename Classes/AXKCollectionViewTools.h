b45a98bdfbc0ecaec2eb0f0a9a5be671e114968e
Alexander Kolov <me@alexkolov.com>
2013-10-30 11:46:30 +0100

    Do not import *.m files

--- Classes/AXKCollectionViewTools.h
+++ Classes/AXKCollectionViewTools.h
@@ -7,12 +7,7 @@
 //
 
 #import "UICollectionReusableView+ReuseIdentifier.h"
-#import "UICollectionReusableView+ReuseIdentifier.m"
 #import "UICollectionViewCell+IndexPath.h"
-#import "UICollectionViewCell+IndexPath.m"
 #import "UICollectionViewCell+ReuseIdentifier.h"
-#import "UICollectionViewCell+ReuseIdentifier.m"
 #import "UITableViewCell+IndexPath.h"
-#import "UITableViewCell+IndexPath.m"
 #import "UITableViewCell+ReuseIdentifier.h"
-#import "UITableViewCell+ReuseIdentifier.m"
